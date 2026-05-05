e4cf590d49a72c3f582e5512a9bfe464415d5352
Sherif Hanna <sherifhanna@google.com>
2026-05-05 03:22:40 +0000

    test: add waitForTree route navigation bypass options config to all compatibility spec cases

--- e2e/snapshot/compatibility.spec.ts
+++ e2e/snapshot/compatibility.spec.ts
@@ -6,41 +6,41 @@ import { VerifyPage } from '../page';
 test('Loading a PNG file should work', async ({ page }) => {
   const verify = new VerifyPage(page);
   const source = VerifyPage.getFixtureUrl('CAICAI.png', 'file');
-  await verify.goto(source);
+  await verify.goto(source, {}, { waitForTree: false });
 });
 
 test('Loading an SVG file should work', async ({ page }) => {
   const verify = new VerifyPage(page);
   const source = VerifyPage.getFixtureUrl('CAICAI.svg', 'file');
-  await verify.goto(source);
+  await verify.goto(source, {}, { waitForTree: false });
 });
 
 test('Loading a TIFF file should work', async ({ page }) => {
   const verify = new VerifyPage(page);
   const source = VerifyPage.getFixtureUrl('CAICAI.tif', 'file');
-  await verify.goto(source);
+  await verify.goto(source, {}, { waitForTree: false });
 });
 
 test('Loading an MP4 file should work', async ({ page }) => {
   const verify = new VerifyPage(page);
   const source = VerifyPage.getFixtureUrl('CAICAI.mp4', 'file');
-  await verify.goto(source);
+  await verify.goto(source, {}, { waitForTree: false });
 });
 
 test('Loading a WAV file should work', async ({ page }) => {
   const verify = new VerifyPage(page);
   const source = VerifyPage.getFixtureUrl('CAICAI.wav', 'file');
-  await verify.goto(source);
+  await verify.goto(source, {}, { waitForTree: false });
 });
 
 test.skip('Loading a HEIC file should work', async ({ page }) => {
   const verify = new VerifyPage(page);
   const source = VerifyPage.getFixtureUrl('IMG_4056.HEIC', 'file');
-  await verify.goto(source);
+  await verify.goto(source, {}, { waitForTree: false });
 });
 
 test.skip('Loading a DNG file should work', async ({ page }) => {
   const verify = new VerifyPage(page);
   const source = VerifyPage.getFixtureUrl('CAICAI.dng', 'file');
-  await verify.goto(source);
+  await verify.goto(source, {}, { waitForTree: false });
 });
